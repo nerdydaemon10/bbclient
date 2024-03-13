9879e7ac0f286561ce4679d8d2ecbb69d5a4613e
nerdydaemon10 <keannoregino@gmail.com>
2024-03-13 21:01:01 +0800

    neww

--- src/contexts/DashboardProvider.jsx
+++
@@ -1,7 +0,0 @@
-import { createContext, useState } from "react"
-
-const SidebarContext = createContext()
-
-function SidebarProvider({children}) {
-  const [currentSidebarItem, setCurrentSidebarItem] = useState("")
-}
