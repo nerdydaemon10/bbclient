c12aaad4628ed7bfa929006f06d9b3ad33ed0b20
Chester <89991461+ChesterJaz@users.noreply.github.com>
2024-03-15 13:11:37 +0800

    added dynamic user

--- src/pages/Home/views/pos/PosTitleSection.jsx
+++ src/pages/Home/views/pos/PosTitleSection.jsx
@@ -1,8 +1,12 @@
+import { useSelector } from "react-redux"
+
+
 function PosTitleSection() {
+  const { user } = useSelector(state => state.auth)
   return (
     <div className="pos-title-section">
       <h3 className="mb-0">POS System</h3>
-      <p className="mb-0">Goodmorning Keanno, Welcome Back!</p>
+      <p className="mb-0">Hello {user.user.username}, Welcome Back!</p>
     </div>
   )
 }
