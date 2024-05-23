d2205993706abce888572473c7a498300f294d5d
nerdydaemon10 <keannoregino@gmail.com>
2024-05-23 12:31:49 +0800

    newww

--- src/ui/common/receipt/ReceiptList.jsx
+++ src/ui/common/receipt/ReceiptList.jsx
@@ -15,8 +15,8 @@ function ReceiptList({className, receipts}) {
       <ul className={`receipt-list d-flex flex-column justify-content-center list-unstyled m-0 border rounded ${className}`}>
         {receipts.filter(receipt => !isEmpty(receipt)).map((receipt, index) => (
         <li key={index} className="receipt-list-item d-flex flex-row justify-content-between gap-2 p-1 border-bottom">
-            <span className="fs-7 fw-medium">{truncate(receipt.label, 24)}:</span>
-            <span className="fs-7 fw-normal">{format(receipt)}</span>
+            <span className="fs-6 fw-normal">{truncate(receipt.label, 24)}:</span>
+            <span className="fs-6 fw-medium">{format(receipt)}</span>
           </li>
         ))}
       </ul>
